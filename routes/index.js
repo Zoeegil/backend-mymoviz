a1a003d16ebbcf7d2420a08dd80dc93210aa74a3
Zoé <gilzoe7@gmail.com>
2024-10-02 16:52:03 +0200

    repertoire

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch');
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 router.get('/movies', (req, res) => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
+        fetch(`https://api.themoviedb.org/3/movie/550?api_key=${TMDB_API_KEY}`)
  .then(response => response.json())
  .then(data => {
     console.log(data)
